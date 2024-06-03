877e0bdc997d9f1de3108e81f4a337e4ec6770d2
Ose <ose.sunday@gmail.com>
2024-06-03 00:19:49 -0400

    render rout

--- index.js
+++ index.js
@@ -1,44 +1,39 @@
-const express = require ('express');
-const dotenv = require ('dotenv');
+const express = require('express');
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-
-
 
 const blogsRoute = require('./routes/blogsRoute.js');
 
+const app = express();
+dotenv.config();
 
-
-const app =express();
-dotenv.config()
-
-
-
-const connect = async ()=>{
+const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGO);
         console.log("connected to mongoDB");
-      } catch (error) {
-        throw error
-      }
-    };  
-    
-    mongoose.connection.on("disconnected", ()=> {
-        console.log("mongoDB disconnected")
-    });
+    } catch (error) {
+        throw error;
+    }
+};
 
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected");
+});
 
-//middlewares
-app.use(express.json())
+// Middlewares
+app.use(express.json());
 
 app.use("/api/v1/blogs", blogsRoute);
 
+// Route for root URL
+app.get('/', (req, res) => {
+    res.send('Welcome to the Blog API');
+});
 
+// Starting server
+const port = process.env.PORT || 3003;
 
-
-//starting server
-const port = 3003
-
-app.listen(port, ()=> {
-    connect()
-    console.log(`App's running on port ${port}...`)
+app.listen(port, () => {
+    connect();
+    console.log(`App's running on port ${port}...`);
 });
